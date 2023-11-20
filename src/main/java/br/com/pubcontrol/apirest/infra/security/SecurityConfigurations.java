069d2bc0c7b7befc84a5145deed333fb718f8365
vitorZu <vitor.zucon25@gmail.com>
2023-11-19 22:29:55 -0300

    back to ADMIN role to register

--- src/main/java/br/com/pubcontrol/apirest/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/pubcontrol/apirest/infra/security/SecurityConfigurations.java
@@ -28,7 +28,7 @@ public class SecurityConfigurations {
                         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                         .authorizeHttpRequests(authorize -> authorize
                                                                     .requestMatchers(HttpMethod.POST, "auth/login").permitAll()
-                                                                    .requestMatchers(HttpMethod.POST, "auth/register").permitAll()
+                                                                    .requestMatchers(HttpMethod.POST, "auth/register").hasRole("ADMIN")
                                                                     .anyRequest().authenticated()
                                                                     )
                                                                 .addFilterBefore(securityFilter,UsernamePasswordAuthenticationFilter.class)
